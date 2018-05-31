8eb438791d1e4eb9f90086284a4df89a475e736f
mike stedman <barbeque@users.noreply.github.com>
2018-05-31 12:13:45 -0600

    Stop trying to use the MacOS -L on Linux/Travis

--- build.rs
+++ build.rs
@@ -1,4 +1,10 @@
+#[cfg(target_os = "macos")]
 fn main() {
     // For Mac only, point the linker at /Library/Frameworks
     println!("cargo:rustc-link-search=framework=/Library/Frameworks")
+}
+
+#[cfg(not(target_os = "macos"))]
+fn main() {
+
 }
